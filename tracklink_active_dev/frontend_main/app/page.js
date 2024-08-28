c2ee4f4f3699a6978429f51e2f888cd819e0b72e
Dan <danvb10@gmail.com>
2024-08-28 00:21:55 -0600

    Add toolpad studio

--- tracklink_active_dev/frontend_main/app/page.js
+++ tracklink_active_dev/frontend_main/app/page.js
@@ -1,11 +1,23 @@
-import styles from "./page.module.css";
+import {Grid2} from "@mui/material";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <span>test</span>
-      </div>
+    <main>
+
+        <Grid2 container spacing={2}>
+            <Grid2 size={8}>
+                <span>Tracklink</span>
+            </Grid2>
+            <Grid2 size={4}>
+                <span>Tracklink</span>
+            </Grid2>
+            <Grid2 size={4}>
+                <span>Tracklink</span>
+            </Grid2>
+            <Grid2 size={8}>
+                <span>Tracklink</span>
+            </Grid2>
+        </Grid2>
     </main>
   );
 }
