241a12d2f6714381803019fc6a3962d0f66bf6f7
Dan <danvb10@gmail.com>
2024-11-06 23:49:17 +0100

    Future notes for root routing

--- tracklink_active_dev/tracklink-frontend-mbp/app/page.js
+++ tracklink_active_dev/tracklink-frontend-mbp/app/page.js
@@ -4,7 +4,9 @@ export default function Home() {
 
     return (
         <main>
-            <h2>Root</h2>
+            <h2><code>`/`</code> Page</h2>
+            <p>This is the landing page for unauthenticated sessions.</p>
+            <p>It will redirect to <code>/home</code> during an authenticated session.</p>
         </main>
     );
 }
