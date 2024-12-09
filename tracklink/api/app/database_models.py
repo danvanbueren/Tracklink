242d3c01f16139ee891e6cf9d59ce5cb1c2b96fc
Dan <danvb10@gmail.com>
2024-12-10 00:11:42 +0100

    Build out db tables

--- tracklink/api/app/database_models.py
+++ tracklink/api/app/database_models.py
@@ -13,17 +13,24 @@ from datetime import datetime, timezone
 Base = declarative_base()
 
 class UsersTable(Base):
-    __tablename__ = "users"
-    id = Column(Integer, primary_key=True, index=True)
+    __tablename__ = "users_table"
+    pkey_id = Column(Integer, primary_key=True, index=True, autoincrement=True) #Int
+    email = Column(String, unique=True, index=True) #String
+    password_hash = Column(String) #String
+    display_name = Column(String, unique=True, index=True) #String
+    default_privacy_type = Column(String, index=True) #String: private, public, friends, friends_of_friends
+    last_active = Column(DateTime, default=datetime.now(timezone.utc)) #DateTime
 
-    email = Column(String, unique=True, index=True)
-    hashed_password = Column(String)
-
-    display_name = Column(String, unique=True, index=True)
+class TracksTable(Base):
+    __tablename__ = "tracks_table"
+    pkey_id = Column(Integer, primary_key=True, index=True, autoincrement=True) #Int
+    fkey_owner = Column(Integer, index=True, nullable=False) #Int
+    track_name = Column(String, index=True, nullable=False) #String
+    privacy_type = Column(String, index=True, nullable=False) #String: private, public, friends, friends_of_friends
 
 class FileMetadataTable(Base):
     __tablename__ = "file_metadata"
-    id = Column(Integer, primary_key=True, index=True)
+    pkey_id = Column(Integer, primary_key=True, index=True, autoincrement=True) #Int
 
     filename = Column(String, index=True, nullable=False)
     filepath = Column(String, nullable=False)
