347790fd53292d99796c3266d5bc8c4f9c74bf95
Dan <danvb10@gmail.com>
2024-12-16 14:02:08 +0200

    Token validity extended with `/session/validate`

--- tracklink/api/app/authLogic.py
+++ tracklink/api/app/authLogic.py
@@ -41,6 +41,19 @@ def verify_password(plain_password, hashed_password):
 def get_password_hash(plain_password):
     return pwd_context.hash(plain_password)
 
+def update_user_last_active(pkey_id: int):
+    try:
+        db = next(get_db())
+        user = db.query(UsersTable).filter(UsersTable.pkey_id == pkey_id).first()
+
+        if user is None:
+            raise HTTPException(status_code=404, detail="update_user_last_active() :: User not found")
+
+        user.last_active = datetime.now(timezone.utc)
+        db.commit()
+    except Exception as e:
+        raise HTTPException(status_code=500, detail="update_user_last_active() :: " + str(e))
+
 def get_user(pkey_id: Optional[int] = None, email: Optional[str] = None, display_name: Optional[str] = None):
     try:
         db = next(get_db())
@@ -52,23 +65,23 @@ def get_user(pkey_id: Optional[int] = None, email: Optional[str] = None, display
         elif display_name:
             user_data = db.query(UsersTable).filter(UsersTable.display_name == display_name).first()
         else:
-            raise HTTPException(status_code=500, detail="Programmatic error: No user data provided")
+            raise HTTPException(status_code=500, detail="get_user() :: Programmatic error: No user data provided")
 
         if user_data is None:
-            raise HTTPException(status_code=404, detail="User not found")
+            raise HTTPException(status_code=404, detail="get_user() :: User not found")
 
         return user_data
     except Exception as e:
-        raise e
+        raise 'get_user() :: ' + str(e)
 
 def authenticate_user(email: str, password: str):
     user = get_user(email=email)
 
     if not user:
-        raise HTTPException(status_code=404, detail="User not found")
+        raise HTTPException(status_code=404, detail="authenticate_user() :: User not found")
 
     if not verify_password(password, user.password_hash):
-        raise HTTPException(status_code=401, detail="Failed to authenticate")
+        raise HTTPException(status_code=401, detail="authenticate_user() :: Failed to authenticate")
 
     return user
 
@@ -80,9 +93,8 @@ def create_access_token(data: dict, expires_delta: timedelta | None = None):
 
     try:
         json.dumps(to_encode)  # Validate if data is JSON serializable
-    except (TypeError, ValueError) as e:
-        print(f"Non-serializable data encountered: {e}")  # Detailed logging
-        raise ValueError("to_encode contains non-serializable data")
+    except Exception as e:
+        raise 'create_access_token() :: ' + str(e)
 
     if expires_delta:
         expire = datetime.now(timezone.utc) + expires_delta
@@ -100,22 +112,22 @@ async def get_user_from_token_including_disabled(token: str = Depends(oauth2_sch
         email: str = payload.get("sub")
 
         if email is None:
-            raise HTTPException(status_code=401, detail="Failed to authenticate")
+            raise HTTPException(status_code=401, detail="get_user_from_token_including_disabled() :: Malformed payload")
 
         token_data = TokenData(email=email)
-    except:
-        raise HTTPException(status_code=401, detail="Failed to authenticate")
+    except Exception as e:
+        raise 'get_user_from_token_including_disabled() :: ' + str(e)
 
     user = get_user(email=token_data.email)
 
     if user is None:
-        raise HTTPException(status_code=401, detail="Failed to authenticate")
+        raise HTTPException(status_code=401, detail="get_user_from_token_including_disabled() :: User not found despite decoded token")
 
     return user
 
 async def get_user_from_token(current_user: UserInDB = Depends(get_user_from_token_including_disabled)):
     if current_user.disabled:
-        raise HTTPException(status_code=400, detail="Inactive user")
+        raise HTTPException(status_code=400, detail="get_user_from_token() :: User account is disabled")
 
     return current_user
 
--- tracklink/api/app/routes/auth.py
+++ tracklink/api/app/routes/auth.py
@@ -5,13 +5,14 @@
 # https://github.com/danvanbueren/Tracklink/blob/main/LICENSE
 
 """Authentication endpoints."""
-from datetime import timedelta
+from datetime import timedelta, datetime, timezone
 
 from fastapi import APIRouter, Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy import inspect
 
-from app.authLogic import authenticate_user, ACCESS_TOKEN_EXPIRE_MINUTES, create_access_token, get_user_from_token
+from app.authLogic import authenticate_user, ACCESS_TOKEN_EXPIRE_MINUTES, create_access_token, get_user_from_token, \
+    update_user_last_active
 from app.pydantic_models import Token, User
 
 router = APIRouter()
@@ -21,10 +22,12 @@ async def create_token_from_login(form_data: OAuth2PasswordRequestForm = Depends
     user = authenticate_user(form_data.username, form_data.password)
 
     if not user:
-        raise HTTPException(status_code=401, detail="Failed to authenticate")
+        raise HTTPException(status_code=401, detail="create_token_from_login() :: Failed to authenticate")
 
     access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     access_token = create_access_token(data={"sub": user.email}, expires_delta=access_token_expires)
+
+    update_user_last_active(user.pkey_id)
 
     result = {
         "access_token": access_token,
@@ -34,18 +37,36 @@ async def create_token_from_login(form_data: OAuth2PasswordRequestForm = Depends
     return result
 
 @router.get("/session/validate", summary="Check if the current session is still valid.")
-async def validate_session(current_user: User = Depends(get_user_from_token)):
+async def validate_and_extend_session(current_user: User = Depends(get_user_from_token)):
+    try:
+        now = datetime.now(timezone.utc)
 
-    valid_until = '?'
+        if current_user.last_active.tzinfo is None:
+            last_active_utc = current_user.last_active.replace(tzinfo=timezone.utc)
+        else:
+            last_active_utc = current_user.last_active
 
-    return {
-        "token_valid": True,
-        "valid_until": valid_until,
-        "user": {
-            c.key: getattr(current_user, c.key)
-            for c in inspect(current_user).mapper.column_attrs
-        }
-    }
+        session_expires = last_active_utc + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+        token_valid = session_expires >= now
+
+        if token_valid:
+            update_user_last_active(current_user.pkey_id)
+
+            return {
+                "token_valid": token_valid,
+                "last_active": current_user.last_active.isoformat(),
+                "valid_until": session_expires.isoformat(),
+                "user": {
+                    c.key: getattr(current_user, c.key)
+                    for c in inspect(current_user).mapper.column_attrs
+                }
+            }
+        else:
+            return {
+                "token_valid": token_valid
+            }
+    except Exception as e:
+        raise e
 
 @router.get("/current", response_model=User)
 async def get_current_user_information(current_user: User = Depends(get_user_from_token)):
