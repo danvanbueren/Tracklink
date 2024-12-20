15308a9df17bd2a6d869611c7df3c7fced2eedf1
Dan <danvb10@gmail.com>
2024-12-20 15:18:39 +0200

    Landing skeleton

--- tracklink/frontend/components/views/unauth/LandingPage.js
+++ tracklink/frontend/components/views/unauth/LandingPage.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import {Button, Card} from "@mui/material";
+import {Button, Card, CardContent, Skeleton, Typography} from "@mui/material";
 import {useRoute} from "@/context/RouteContext";
 import Grid from "@mui/material/Grid2";
 
@@ -15,23 +15,46 @@ export default function LandingPage({ slug }) {
                 <Grid size='grow' />
 
                 <Grid size='auto'>
+                    <Card
+                        sx={{
+                            padding: '0rem',
+                            marginTop: '20vh',
+                    }}
+                    >
 
-                    <h1>Welcome to Tracklink</h1>
+                        <Skeleton
+                            variant="rectangular"
+                            animation="wave"
+                            sx={{
+                                height: '15vh',
+                                width: '100%'
+                            }}
+                        />
 
-                    <h4>Tracklink description</h4>
-
-                    <Card sx={{}}>
-                        <Button
-                            fullWidth
-                            variant='contained'
-                            onClick={() => {
-                                navigate('login')
+                        <CardContent
+                            sx={{
+                                padding: '2rem',
+                                display: 'flex',
+                                flexDirection: 'column',
+                                alignItems: 'center',
                             }}
                         >
-                            Get Started
-                        </Button>
+                            <Typography variant='h3' sx={{ fontWeight: 'bold' }}>Welcome to Tracklink</Typography>
+                            <Skeleton animation="wave">
+                                <Typography variant='h6' sx={{ fontSize: '1rem', marginTop: '1rem', marginBottom: '2rem' }}>description</Typography>
+                            </Skeleton>
+                            <Button
+                                fullWidth
+                                variant='contained'
+                                size='large'
+                                onClick={() => {
+                                    navigate('login')
+                                }}
+                            >
+                                Get Started
+                            </Button>
+                        </CardContent>
                     </Card>
-
                 </Grid>
 
                 <Grid size='grow'/>
